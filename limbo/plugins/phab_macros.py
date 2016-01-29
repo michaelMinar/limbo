6e40ec9b575938d13f5595b8993b61b9234bab4e
michaelMinar <mminar@trifacta.com>
2016-01-28 22:31:49 -0800

    cleans up comments

--- limbo/plugins/phab_macros.py
+++ limbo/plugins/phab_macros.py
@@ -13,16 +13,24 @@ PHAB_FILE_ENDPOINT = 'http://' + os.environ['PHAB_FILE_HOST']
 phab = Phabricator(host=PHAB_API_ENDPOINT, token=PHAB_API_TOKEN)
 
 def fetch_macro_uris(names):
-    """This function fetches macro URIs, Mike"""
+    """This function fetches macro URIs
+
+    :param list names: phab macro names
+    :returns: (*list*) -- uris for each name
+    """
     return [m['uri'] for m in phab.macro.query(names=names).values()]
 
 def find_macro_names(text):
-    """This function finds macro names in the given text, Mike"""
+    """This function finds macro names in the given text
+
+    :param str text:
+    :returns: (*list*) -- list of phab macro names pulled from input string
+    """
     return map(lambda name: name.lower(),
                re.findall('(' + '|'.join(phab.macro.query().keys()) + ')', text, re.IGNORECASE))
 
 def on_message(msg, server):
-    """This function handles messages, Mike"""
+    """This function handles messages"""
     found_macros = find_macro_names(msg.get("text", ""))
     if found_macros:
         return ' '.join(map(lambda uri: re.sub('^' + PHAB_ENDPOINT, PHAB_FILE_ENDPOINT, uri),
