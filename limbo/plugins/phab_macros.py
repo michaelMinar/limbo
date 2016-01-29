da1a8bbc0a4c6583848de95e761535f57d7dfa61
Eli Marschner <eli@trifacta.com>
2016-01-28 20:39:35 -0800

    add phab macro plugin

---
+++ limbo/plugins/phab_macros.py
@@ -0,0 +1,26 @@
+"""Translate phabricator macro names into unfurl-able URLs"""
+
+from phabricator import Phabricator
+import os
+import re
+
+PHAB_HOST = os.environ['PHAB_HOST']
+PHAB_ENDPOINT = 'https://' + PHAB_HOST
+PHAB_API_ENDPOINT = 'https://' + PHAB_HOST + '/api/'
+PHAB_API_TOKEN = os.environ['PHAB_API_TOKEN']
+PHAB_FILE_ENDPOINT = 'http://' + os.environ['PHAB_FILE_HOST']
+
+phab = Phabricator(host=PHAB_API_ENDPOINT, token=PHAB_API_TOKEN)
+
+def fetch_macro_uris(names):
+    return [m['uri'] for m in phab.macro.query(names=names).values()]
+
+def find_macros(text):
+    return map(lambda name: name.lower(),
+               re.findall('(' + '|'.join(phab.macro.query().keys()) + ')', text, re.IGNORECASE))
+
+def on_message(msg, server):
+    found_macros = find_macros(msg.get("text", ""))
+    if found_macros:
+        return ' '.join(map(lambda uri: re.sub('^' + PHAB_ENDPOINT, PHAB_FILE_ENDPOINT, uri),
+                            fetch_macro_uris(found_macros)))
