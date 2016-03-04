23d7203a90c1622703870c31803c7423e787ecb4
michaelMinar <mminar@trifacta.com>
2016-03-03 23:39:57 -0800

    adds a dict for custom trigger mappings. frees us from phab constraints

--- limbo/plugins/phab_macros.py
+++ limbo/plugins/phab_macros.py
@@ -19,6 +19,10 @@ PHAB_FILE_ENDPOINT = 'http://' + os.environ['PHAB_FILE_HOST']
 
 phab = Phabricator(host=PHAB_API_ENDPOINT, token=PHAB_API_TOKEN)
 
+CUSTOM_MAP = {
+    'nerd rage': 'nerdrage'
+}
+
 def find_macro_names(text):
     """Message text comes in, macro names get fetched, and the ones that match go out as a list
 
@@ -26,7 +30,11 @@ def find_macro_names(text):
     :returns: (*list*) -- list of phab macro names pulled from input string
     """
     wb = r'(\w+\s+|\b|\W|^)'  # "Word Boundary"
-    names = r'(' + r'|'.join(phab.macro.query().keys()) + r')'
+
+    macro_keys = phab.macro.query().keys()
+    complete_keys = macro_keys + CUSTOM_MAP.keys()
+    names = r'(' + r'|'.join(complete_keys) + r')'
+
     return map(lambda name: (name[2] or name[4]).lower(),
                re.findall(r'(' + wb + names + wb + r'|^[\s]*' + names + r'\s*$)',
                           text, re.IGNORECASE | re.MULTILINE))
@@ -38,7 +46,20 @@ def fetch_macro_uris(names):
     :param list names: phab macro names
     :returns: (*list*) -- uris for each name
     """
-    return [m['uri'] for m in phab.macro.query(names=names).values()]
+    proper_names = map(swap_back_to_proper_name, names)
+    return [m['uri'] for m in phab.macro.query(names=proper_names).values()]
+
+def swap_back_to_proper_name(found_name):
+    """Takes a found name and checks if it was a custom map. If found there,
+    we swap back the proper phab macro name. Else we just return the found name.
+
+    :param str found_name:
+    :returns: (*str*)
+    """
+    if found_name in CUSTOM_MAP.keys():
+        return CUSTOM_MAP[found_name]
+    else:
+        return found_name
 
 def on_message(msg, server):
     """Macro URIs from message get rewritten for the file proxy and returned separated by newlines
